16156c1a5ba4b361816b87837e3f9b8292b38d23
Nabende Brian <briannabende1@gmail.com>
2025-04-08 14:56:32 +0300

    few changes

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,34 +1,32 @@
-const pool = require("../database/")
+const pool = require("../database/");
 
 /* ***************************
  *  Get all classification data
- * ************************** */
-async function getClassifications(){
-  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+ *************************** */
+async function getClassifications() {
+  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name");
 }
-
-
-
-
 
 /* ***************************
  *  Get all inventory items and classification_name by classification_id
- * ************************** */
+ *************************** */
 async function getInventoryByClassificationId(classification_id) {
   try {
     const data = await pool.query(
       `SELECT * FROM public.inventory AS i 
-      JOIN public.classification AS c 
-      ON i.classification_id = c.classification_id 
-      WHERE i.classification_id = $1`,
+       JOIN public.classification AS c 
+       ON i.classification_id = c.classification_id 
+       WHERE i.classification_id = $1`,
       [classification_id]
-    )
-    return data.rows
+    );
+    return data.rows;
   } catch (error) {
-    console.error("getclassificationsbyid error " + error)
+    console.error("getInventoryByClassificationId error: " + error);
+    throw error; // re-throw for error middleware to catch
   }
 }
 
-module.exports = {getClassifications}
-
-module.exports = {getClassifications, getInventoryByClassificationId};
+module.exports = {
+  getClassifications,
+  getInventoryByClassificationId,
+};
